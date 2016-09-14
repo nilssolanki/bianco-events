4cf57c095c93c7dd9e95873e6b98bce75e25332b
nilssolanki <hibatsu@solanki.ch>
2016-09-14 11:05:26 +0200

    Add option for once listeners, add a listener list
    The listener list keeps track of all the current listeners
    and allows for removing all listeners for a certain event at once.

--- index.next.js
+++ index.next.js
@@ -1,4 +1,9 @@
 /**
+ * The list of listeners, using DOM nodes as keys
+ */
+const listeners = new WeakMap()
+
+/**
  * Split a string into several items separed by spaces
  * @param   { String } list - events list
  * @returns { Array } all the events detected
@@ -11,11 +16,32 @@ const split = list => list.split(/\s/)
  * @param   { String }      evList - list of events we want to bind or unbind space separated
  * @param   { Function }    cb     - listeners callback
  * @param   { String }      method - either 'addEventListener' or 'removeEventListener'
+ *  @param   { Object }      options - the event listener options
  */
-function manageEvents(el, evList, cb, method) {
+function manageEvents(el, evList, cb, method, options) {
+  if (!listeners.has(el)) {
+    listeners.set(el, {});
+  }
+  
+  const elListeners = listeners.get(el);
+  
   split(evList).forEach((e) => {
-    el[method](e, cb, false)
+    el[method](e, cb, false, options)
+    manageListeners(elListeners, e, cb, method === 'addEventListener')
   })
+}
+
+function manageListeners(elListeners, evList, cb, isAdd) {
+  const eventListeners = elListeners[evList] = elListeners[evList] || []
+  
+  if (isAdd) {
+    eventListeners.push(cb)
+  } else if (cb) {
+    const cbIndex = eventListeners.indexOf(cb)
+    eventListeners.splice(cbIndex, 1)
+  } else {
+    Reflect.deleteProperty(elListeners, evList);
+  }
 }
 
 /**
@@ -25,8 +51,8 @@ function manageEvents(el, evList, cb, method) {
  * @param   { Function }    cb     - listeners callback
  * @returns { HTMLElement } DOM node and first argument of the function
  */
-export function add(el, evList, cb) {
-  manageEvents(el, evList, cb, 'addEventListener')
+export function add(el, evList, cb, options) {
+  manageEvents(el, evList, cb, 'addEventListener', options)
   return el
 }
 
@@ -37,13 +63,11 @@ export function add(el, evList, cb) {
  * @param   { Function }    cb     - listeners callback
  * @returns { HTMLElement } DOM node and first argument of the function
  */
-export function once(el, evList, cb) {
-  var handleEvent = function() {
-    cb.apply(el, [el, evList, handleEvent])
-    remove(el, evList, handleEvent)
-  }
-
-  manageEvents(el, evList, handleEvent, 'addEventListener')
+export function once(el, evList, cb, options) {
+  options = Object.assign(options, {
+    once: true
+  })
+  manageEvents(el, evList, cb, 'addEventListener', options)
   return el
 }
 
